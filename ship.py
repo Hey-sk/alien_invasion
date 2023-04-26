7e57cee79075d2812c020deb6229e8d71db96886
hey-sk <stephenakeane@gmail.com>
2023-04-25 21:50:32 -0400

    ship logic complete

--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
         '''initialize screen and starting position'''
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         #load ship and get its rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -12,6 +13,25 @@ class Ship:
 
         #start each new ship at the bottom center of screen
         self.rect.midbottom = self.screen_rect.midbottom
+        
+        # store a float for the ships exact horizontal position
+        self.x = float(self.rect.x)
+
+        #movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        '''update ship movement based on movement flag'''
+
+        #update the ship's x value- not the rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+ 
+        #update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         #draw the screen in its current locaton
